e4292c0f31e2cdd0fb78d2e655c9ab9fe53f6da8
sarojchhetri77 <=>
2024-03-18 23:42:34 +0545

    model,migration created

---
+++ app/Models/Blogs.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blogs extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'category_id',
+        'title',
+        'description',
+        'image',
+    ];
+
+    public function category(){
+        return $this->belongsTo(category::class);
+    }
+}
